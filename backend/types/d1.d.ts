4f1e9dadf672bcab28b31be6c9ee0f40c4866581
Kosei Masuda <kouseikiyo0725@gmail.com>
2026-03-13 00:13:55 +0900

    deleted d1databse type

--- backend/types/d1.d.ts
+++
@@ -1,8 +0,0 @@
-// src/types/d1.d.ts などに置く
-export interface D1Database {
-  prepare(query: string): {
-    all(): Promise<{ results: any[] }>;
-    get(): Promise<any>;
-    run(): Promise<void>;
-  };
-}
