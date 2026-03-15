05b7e93347535db5a8289cbdd3a86266ecc60430
Kosei Masuda <kouseikiyo0725@gmail.com>
2026-03-15 20:41:43 +0900

    fixed sql

--- backend/src/services/bookingService.ts
+++ backend/src/services/bookingService.ts
@@ -11,9 +11,31 @@ export const getBookings = async (c: Context<{ Bindings: CloudflareBindings }>) 
   const userId = user.sub
 
   const { results } = await c.env.DB
-    .prepare("SELECT * FROM bookings WHERE host = ?")
-    .bind(userId)
+    .prepare(`
+      SELECT 
+        bookings.id,
+        bookings.guest_name,
+        bookings.start,
+        bookings.end,
+        bookings.meet_url,
+        event_types.title
+      FROM bookings
+      JOIN event_types
+        ON bookings.event_type_id = event_types.id
+    `)
+    //.bind(userId)
     .all()
 
   return c.json(results)
 }
+      // SELECT 
+      //   bookings.id,
+      //   bookings.guest_name,
+      //   bookings.start,
+      //   bookings.end,
+      //   bookings.meet_url,
+      //   event_types.title
+      // FROM bookings
+      // JOIN event_types
+      //   ON bookings.event_type_id = event_types.id
+      // WHERE event_types.user_id = ?
