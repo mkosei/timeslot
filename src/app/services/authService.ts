7dd662eb3ed566a84083f3603f52257a55069047
Kosei Masuda <kouseikiyo0725@gmail.com>
2026-03-22 22:41:24 +0900

    fixed login func

--- src/app/services/authService.ts
+++ src/app/services/authService.ts
@@ -10,27 +10,8 @@ export async function fetchSession(): Promise<Session>  {
 }
 
 export const login = async () => {
-  const res = await fetch(`${API_URL}/api/auth/csrf`, {
-    credentials: 'include',
-  })
-  const data = await res.json() as { csrfToken: string }
-
-  const form = document.createElement('form')
-  form.method = 'POST'
-  form.action = `${API_URL}/api/auth/signin/google`
-
-  const csrfInput = document.createElement('input')
-  csrfInput.name = 'csrfToken'
-  csrfInput.value = data.csrfToken
-  form.appendChild(csrfInput)
-
-  const callbackInput = document.createElement('input')
-  callbackInput.name = 'callbackUrl'
-  callbackInput.value = APP_URL
-  form.appendChild(callbackInput)
-
-  document.body.appendChild(form)
-  form.submit()
+  const callbackUrl = encodeURIComponent(APP_URL + "/schedule")
+  window.location.href = `${API_URL}/api/auth/signin/google?callbackUrl=${callbackUrl}`
 }
 
 export const logout = () => {
